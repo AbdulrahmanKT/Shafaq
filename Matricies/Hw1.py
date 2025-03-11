000099d9d836e8fcac99e142f1f4a2869854278b
abdulrahman <ultimapanda65@gmail.com>
2025-03-11 23:50:12 +0300

    Fixed namings for homework submission

---
+++ Matricies/Hw1.py
@@ -0,0 +1,36 @@
+import SBP as sb 
+import matplotlib.pyplot as plt 
+import matplotlib
+#matplotlib.use('Qt5Agg')
+import numpy as np 
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+### Up to 9 significant digits!!!!!!
+
+#Matrix P
+n = 4
+out = sb.lgl(n)
+roots = np.zeros(n+1)
+w = np.zeros(n+1)
+w[:] = out[1,:]
+roots[:] = out[0,:]
+result = np.zeros((n+1,n+1))
+l1 = np.zeros(n+1)
+for i in range(n+1):
+    result = result + (np.outer(sb.lagrange(n,roots[i]),sb.lagrange(n,roots[i])))*w[i]
+P = -1*result # The negative one stems from the formulation of the code. The points were sorted from right to left, hence the jacobian is negative. 
+print("The matrix P \n", result)
+
+
+#Matrix Q from P
+dq = sb.dlagrange(n)
+result1 = np.zeros_like(result)
+for i in range(n+1):
+    result1[:,i] = sb.lagrange(n,roots[i]) @ np.transpose(dq)* w[i]
+result1 = result1  
+Q = result1
+print("The matrix Q from P \n", Q) 
+
+# Matrix D 
+D = sb.dlagrange(n)
+print("The matrix D \n", D) 
--- Matricies/Hw2.py
+++ Matricies/Hw2.py
@@ -21,8 +21,9 @@ y_1 = np.ones_like(x)
 print(np.shape(y_1))
 # Defining a linear functions
 m = 1.5
-y_2 = [x[i]**5 for i in range(n+1)]
-dy2 = [5*x[i]**4 for i in range(n+1)]
+y_2 = [m*x[i] for i in range(n+1)]
+y_3 = [x[i]**2 for i in range(n+1)]
+
 
 
 ### Numerical Differentiation 
@@ -32,31 +33,27 @@ print(D)
 print(y_2 , "/n", D@y_2)
 # 
 # 
- # P 
+# P 
 y_1_d = D@y_1
 y_2_d = D@y_2
 # Create figure and axis
 fig, ax = plt.subplots(figsize=(10, 6))  # Create figure with specified size
 
 # Plot the data
-#ax.plot(x, y_1, 'b-', linewidth=2, label="$y_1$ = 1")
-ax.plot(x, y_2, 'r-', linewidth=2, label="$y_2$ =  $x^5$")
-#ax.plot(x, y_1_d, 'b*', linewidth=2, label="$Dy_1$")
+ax.plot(x, y_1, 'b-', linewidth=2, label="$y_1$ = 1")
+ax.plot(x, y_2, 'r-', linewidth=2, label="$y_2$ =  $mx$")
+ax.plot(x, y_3, 'g-', linewidth=2, label="$y_2= x^2$")
+ax.plot(x, y_1_d, 'b*', linewidth=2, label="$Dy_1$")
 ax.plot(x, y_2_d, 'r*', linewidth=2, label="$Dy_2$")
-ax.plot(x, dy2, 'b-', linewidth=2, label="$Dy_2$")
-
+ax.plot(x, D@y_3, 'g*', linewidth=2, label="$Dy_2$")
 # Set plot labels and title
 ax.set_xlabel('X-axis', fontsize=12)
 ax.set_ylabel('Y-axis', fontsize=12)
-ax.set_title('Plot of y = 1', fontsize=14)
+ax.set_title('Plot of analytical function and numerical derivatives', fontsize=14)
 
 # Add grid, legend and set axis limits
 ax.grid(True, linestyle='--', alpha=0.7)
 ax.legend(fontsize=10)
-ax.set_xlim([-1.5, 1.5])
-ax.set_ylim([-10, 10])
-
-# Improve overall appearance
-#plt.tight_layout()
+ax.set_xlim([-1.01, 1.01])
+ax.set_ylim([-2.2, 2.2])
 plt.show()
-print(x)
